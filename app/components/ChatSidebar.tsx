81d0f4394d200fd6f3486398a714920f4c74e307
gateremark <mark5gatere@gmail.com>
2023-11-16 02:35:11 +0300

    chatsidebar hidden on small screens

--- app/components/ChatSidebar.tsx
+++ app/components/ChatSidebar.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 
 const ChatSidebar = () => {
     return (
-        <div className="bg-[#202123] text-[#ffffff] flex flex-col ">
+        <div className="hidden bg-[#202123] text-[#ffffff] md:flex flex-col ">
             <div className="flex-1 pl-5">Sidebar</div>
             <button className="py-6 inline" onClick={() => signOut()}>
                 Logout
